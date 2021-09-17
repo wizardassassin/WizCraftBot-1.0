52d744a317e2e09d9b464bcab62b461b0d16ede5
wizard_assassin <victorzhai0916@gmail.com>
2021-09-16 23:50:56 -0700

    comments; debugging; hidden .test prefix productivity++

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,7 +1,7 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { Permissions, Collection } from "discord.js";
 import ytdl from "ytdl-core";
-import ytSearch from "yt-search";
+import yts from "yt-search";
 import {
     AudioPlayerStatus,
     StreamType,
@@ -42,13 +42,24 @@ export async function execute(interaction) {
 
     const songInfo = { title: "", url: "", thumbnail: "", duration: "" };
 
+    // gets the content needed to play the video
+    // Video used as a guide: https://www.youtube.com/watch?v=riyHsgI2IDs
+    // TODO: convert all queries to only use yts and download from just ytdl
+    /*
+    videoId = new URL("video-link").searchParams.get("v")
+    video = await yts( { videoId: 'video-id' } )
+    list = await yts( { listId: 'playlist-id' } )
+    list.videos.forEach((video) => {})
+    */
+    let info;
     if (ytdl.validateURL(unparsedSearch)) {
         // If the search is a youtube video URL
         const video = await ytdl.getInfo(unparsedSearch);
+        info = video;
         const seconds = video.videoDetails.lengthSeconds;
         const url = new URL(video.videoDetails.thumbnails[0].url);
-        songInfo.title = url.origin + url.pathname;
-        songInfo.thumbnail = video.videoDetails.title;
+        songInfo.title = video.videoDetails.title;
+        songInfo.thumbnail = url.origin + url.pathname;
         songInfo.url = video.videoDetails.video_url;
         songInfo.duration =
             seconds < 3600
@@ -56,7 +67,7 @@ export async function execute(interaction) {
                 : new Date(seconds * 1000).toISOString().substr(11, 8);
     } else {
         // If it isn't, treat the string as a query
-        const videoResults = await ytSearch(unparsedSearch);
+        const videoResults = await yts(unparsedSearch);
         const video = videoResults.videos[0];
         if (!video) {
             // If the video is not found even with searching
@@ -77,7 +88,24 @@ export async function execute(interaction) {
         adapterCreator: guild.voiceAdapterCreator,
     });
 
-    const stream = ytdl(songInfo.url, { filter: "audioonly" });
+    const maxAudioBitrate = voiceChannel.bitrate / 1000; // kps
+
+    // ECONNRESET ??
+    // AudioOnly files are not playing friendly
+    const formatOptions = {
+        filter: (format) => {
+            if (format.hasAudio && format.audioBitrate <= maxAudioBitrate) {
+                console.log(format.audioBitrate, format.hasVideo);
+                return true;
+            }
+            return false;
+        },
+        quality: "highestaudio",
+    };
+
+    const stream = info
+        ? ytdl.downloadFromInfo(info, formatOptions)
+        : ytdl(songInfo.url, formatOptions);
     const resource = createAudioResource(stream, {
         inputType: StreamType.Arbitrary,
         // inlineVolume: true,
@@ -91,7 +119,7 @@ export async function execute(interaction) {
     connection.subscribe(player);
 
     player.on(AudioPlayerStatus.Idle, () => connection.destroy());
-    //
+    
     await interaction.reply(
         "```json\n" + JSON.stringify(songInfo, null, 4) + "```"
     );
--- test.js
+++
@@ -1,4 +0,0 @@
-import { fileURLToPath } from "url";
-
-console.log(import.meta.url);
-console.log(fileURLToPath(import.meta.url));
